42768cdc31ae5ffc248801a683dff4a501b7ae05
Gerson <gersondalto@gmail.com>
2021-07-14 19:02:10 -0300

    Delete City Tests OK

---
+++ src/main/java/com/devsuperior/bds02/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.devsuperior.bds02.resources.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds02.services.exception.DataBaseException;
+import com.devsuperior.bds02.services.exception.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not Found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+
+	}
+
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> entityNotFound(DataBaseException e, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Data Base Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+
+	}	
+
+}
--- src/main/java/com/devsuperior/bds02/services/CityServices.java
+++ src/main/java/com/devsuperior/bds02/services/CityServices.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exception.DataBaseException;
+import com.devsuperior.bds02.services.exception.ResourceNotFoundException;
 
 @Service
 public class CityServices {
@@ -19,5 +23,17 @@ public class CityServices {
 	public List<CityDTO> findAll() {
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);			
+		}
+		catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id Not Found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity Violation");
+		}
 	}
 }
